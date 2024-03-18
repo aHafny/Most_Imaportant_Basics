c046e0704f0fe3568cea79f27a22d3707b9a2af5
Ahmed Mahmoud Hafny <ahmedmahmoudhafny@gmail.com>
2024-03-18 23:38:42 +0200

    Update sign_of_Number.c

--- sign_of_Number.c
+++ sign_of_Number.c
@@ -2,7 +2,7 @@
  ============================================================================
  Name        : sign_of_Number.c
  Author      : Ahmed Mahmoud Hafny
- Version     : 1.0
+ Version     : 1.01
  Description : program that Checks the sign of the number whether its +ve ,-ve or zero.
  ============================================================================
  */
@@ -17,7 +17,7 @@ int main(void)
 	fflush(stdout);
 	scanf("%d", &num);
 
-	if (num > 0)
+	if (num > 0) 
 	{
 		printf("The Number is Positive");
 	}
@@ -28,7 +28,7 @@ int main(void)
 
 	else
 	{
-		printf("The Number is Zero ");
+		printf("The Number is Zero ");  //Note : if the input is a letter(s) the Output With also Be Zero
 	}
 
 	return 0;
